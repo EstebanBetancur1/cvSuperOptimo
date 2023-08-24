9f8b4fde64e9bd041c0dda929030ec7eeb6c15b1
Esteban Betancur <84148045+EstebanBetancur1@users.noreply.github.com>
2023-08-24 14:42:41 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <script src="{{ asset('js/home.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+    <body>
+        <h1 class="text-9xl uppercase">hola</h1>
+    </body>
+
+</html>
